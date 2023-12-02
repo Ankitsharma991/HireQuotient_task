b64b6a9a27ff32ba4645293b21217e7f4dceab82
Ankit Sharma <sharmaa7861@gmail.com>
2023-12-02 14:37:05 +0530

    completed price and plan component

--- src/Components/PricePlan/PricePlan.js
+++ src/Components/PricePlan/PricePlan.js
@@ -40,6 +40,7 @@ const PricePlan = () => {
             </div>
 
             <div className="card_container">
+              {/* Card-1 */}
               <div className="card1">
                 <div className="card_category">
                   <p className="cat_title">Free</p>
@@ -84,6 +85,120 @@ const PricePlan = () => {
                     <div className="service_points">
                       <img src={check} alt="checked" />
                       <p>A top feature</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* Card-2 */}
+              <div className="card1">
+                <div className="card_category">
+                  <p className="cat_title">Starter</p>
+                  <p className="title_desc">
+                    So you can see how incredible our tool is.
+                  </p>
+                </div>
+
+                <div className="price_details">
+                  <div className="amount_details">
+                    <p className="amount">
+                      {monthly ? "$19" : "$15"}
+                      <span className="amount_mo">/mo</span>
+                    </p>
+                    <p className="highlight_title">
+                      {monthly ? "Billed monthly" : "Billed $180 yearly"}
+                    </p>
+                  </div>
+
+                  <div className="user_action">
+                    <p className="button_start">Get Started</p>
+                    <p className="option_card">
+                      7 days free trial no credit card needed
+                    </p>
+                  </div>
+                </div>
+
+                <div className="services_container">
+                  <p className="service_heading">All Free features, plus:</p>
+                  <div className="services">
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>A cool feature</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>A basic feature</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>A top feature with limitations</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>An incredible feature so useful</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>A top feature</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* Card -3 */}
+              <div className="card1">
+                <div className="card_category">
+                  <p className="cat_title">Pro</p>
+                  <p className="title_desc">
+                    So you can see how incredible our tool is.
+                  </p>
+                </div>
+
+                <div className="price_details">
+                  <div className="amount_details">
+                    <p className="amount">
+                      {monthly ? "$49" : "$45"}
+                      <span className="amount_mo">/mo</span>
+                    </p>
+                    <p className="highlight_title">
+                      {monthly ? "Billed monthly" : "Billed $540 yearly"}
+                    </p>
+                  </div>
+
+                  <div className="user_action">
+                    <p className="button_start">Get Started</p>
+                    <p className="option_card">
+                      7 days free trial no credit card needed
+                    </p>
+                  </div>
+                </div>
+
+                <div className="services_container">
+                  <p className="service_heading">All Starter features, plus:</p>
+                  <div className="services">
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>A cool feature</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>A basic feature</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>A top feature with limitations</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>An incredible feature so useful</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>A premium feature</p>
+                    </div>
+                    <div className="service_points">
+                      <img src={check} alt="checked" />
+                      <p>You need this feature</p>
                     </div>
                   </div>
                 </div>
